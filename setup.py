03f7ff38bfb2752d38cecbbcd29a739a8f6d0f4f
Alberto Ruiz <aruiz@gnome.org>
2010-11-09 23:50:25 +0000

    Added setup.py distutils script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (name="JYPath",
+       version="0.1",
+       description="XPath subset implementation for JSON, YAML and Python collections.",
+       author="Alberto Ruiz",
+       author_email="aruiz@gnome.org",
+       url="http://github.com/aruiz/python-jpath",
+       packages=['jypath'],
+      )
